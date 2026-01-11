cbca93d3d0e3a314dae56db9feffcd3f8fe07083
raihan-778 <raihanarif778@gmail.com>
2026-01-11 18:15:48 +0600

    dynamic page bug fixed

--- app/[location]/page.jsx
+++ app/[location]/page.jsx
@@ -1,11 +1,15 @@
 export const dynamic = "force-dynamic";
 
 import { LocationInfo } from "@/components/LocationInfo";
+import { getResolvedLatLong } from "@/lib/location-info";
 
-const LocationPage = ({
+const LocationPage = async ({
   params: { location },
   searchParams: { latitude, longitude },
 }) => {
-  return <LocationInfo location={location} lat={latitude} lon={longitude} />;
+  const resolved = await getResolvedLatLong(location, latitude, longitude);
+  return (
+    <LocationInfo location={location} lat={resolved.lat} lon={resolved.lon} />
+  );
 };
 export default LocationPage;
